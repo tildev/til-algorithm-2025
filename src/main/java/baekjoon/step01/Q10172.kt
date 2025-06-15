4a6a0f30965efaff94aac9af62de38d01f7020d9
tildev <tildevelove@gmail.com>
2025-06-15 15:29:18 +0900

    feat: Add Kotlin solution for Baekjoon Step 1 - Problem 10172

---
+++ src/main/java/baekjoon/step01/Q10172.kt
@@ -0,0 +1,13 @@
+package baekjoon.step01
+
+fun main() {
+    println(
+        """
+        |\_/|
+        |q p|   /}
+        ( 0 )""${'"'}\
+        |"^"`    |
+        ||_/=\\__|
+    """.trimIndent()
+    )
+}
