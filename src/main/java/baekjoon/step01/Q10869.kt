7a2f6e8d43c49197d543085df7903dc3a5773694
tildev <tildevelove@gmail.com>
2025-06-15 13:50:01 +0900

    feat: Add Kotlin solution for Baekjoon Step 1 - Problem 10869

---
+++ src/main/java/baekjoon/step01/Q10869.kt
@@ -0,0 +1,17 @@
+package baekjoon.step01
+
+import java.util.*
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val st = StringTokenizer(br.readLine())
+
+    val a = st.nextToken().toInt()
+    val b = st.nextToken().toInt()
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+}
