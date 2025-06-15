06a88c2fd6d238734933ff16451bc8e3e440be66
tildev <tildevelove@gmail.com>
2025-06-15 14:53:01 +0900

    feat: Add Kotlin solution for Baekjoon Step 1 - Problem 10926

---
+++ src/main/java/baekjoon/step01/Q10926.kt
@@ -0,0 +1,12 @@
+package baekjoon.step01
+
+import java.util.*
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val st = StringTokenizer(br.readLine())
+
+    val id = st.nextToken().toString()
+
+    println("$id??!")
+}
