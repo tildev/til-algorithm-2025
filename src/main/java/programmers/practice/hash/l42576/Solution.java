8b7d15c1892bbc627dddb28001338192b49e0d55
tildev <tildevelove@gmail.com>
2025-08-06 00:16:27 +0900

    feat: Add solution for programmers - problem 42576

--- src/main/java/programmers/practice/hash/l42576/Solution.java
+++ src/main/java/programmers/practice/hash/l42576/Solution.java
@@ -1,8 +1,10 @@
 package programmers.practice.hash.l42576;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
+    /*
     public String solution(String[] participant, String[] completion) {
         Arrays.sort(participant);
         Arrays.sort(completion);
@@ -13,5 +15,22 @@ public class Solution {
             }
         }
         return participant[participant.length - 1];
+    }
+    */
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> participantMap = new HashMap<>();
+        for (String p : participant) {
+            participantMap.put(p, participantMap.getOrDefault(p, 0) + 1);
+        }
+        for (String c : completion) {
+            participantMap.put(c, participantMap.get(c) - 1);
+        }
+
+        for (String key : participantMap.keySet()) {
+            if (participantMap.get(key) > 0) {
+                return key;
+            }
+        }
+        return "";
     }
 }
