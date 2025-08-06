c0e6f1b6552695fc853b4d97653ba1543e5d0cd4
tildev <tildevelove@gmail.com>
2025-08-06 22:25:10 +0900

    feat: Add solution for programmers - problem 1845

--- src/main/java/programmers/practice/hash/l1845/Solution.java
+++ src/main/java/programmers/practice/hash/l1845/Solution.java
@@ -1,9 +1,9 @@
 package programmers.practice.hash.l1845;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class Solution {
+    /*
     public int solution(int[] nums) {
         int limit = nums.length / 2;
 
@@ -13,5 +13,14 @@ public class Solution {
         }
 
         return Math.min(limit, set.size());
+    }
+    */
+    public int solution(int[] nums) {
+        return (int) Math.min(Arrays.stream(nums).distinct().count(), nums.length / 2);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 3, 3, 4, 5};
+        System.out.println(new Solution().solution(nums));
     }
 }
